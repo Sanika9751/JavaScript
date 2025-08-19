e08eb07d9bd6d8a893a216c61a05f6326516340b
Sanika9751 <sanikakamble0451@gmail.com>
2025-08-19 21:00:56 +0530

    JS Programms

--- RestOp2.js
+++ RestOp2.js
@@ -1,10 +1,11 @@
-// function sum(a,b,...c){
-//     c.reduce(n1,n2)=>
-//     {
-//         return n1+=n2;
-//     };
-    
-// }
+function sum(a,b,...c){
+    let result=c.reduce((n1,n2)=>
+    {
+        n=n1+n2;
+        return n;
+    });
+    console.log(a+b+result);
+}
 
-// console.log(sum(10,20));
-// console.log(sum(10,30,40));
+sum(10,20,30);
+sum(10,30,40);
